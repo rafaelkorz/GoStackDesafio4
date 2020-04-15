a4cd08236287f50c3c78eed66b31c1a8c9764ae1
Rafael Korz <rafael.korz@gmail.com>
2020-04-15 19:07:43 -0300

    Finished challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,13 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransationDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +22,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        accumulator.income +=
+          transaction.type === 'income' ? transaction.value : 0;
+        accumulator.outcome +=
+          transaction.type === 'outcome' ? transaction.value : 0;
+
+        accumulator.total = accumulator.income - accumulator.outcome;
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransationDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const balance = this.getBalance();
+
+    if (type === 'outcome') {
+      if (balance.total < value) {
+        throw Error('Dont have balance!');
+      }
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
